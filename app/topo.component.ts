96ee94a498031b618220496a29fbf5dadec7a15f
Sébastien GLON <glon.sebastien@free.fr>
2016-09-28 07:48:02 +0200

    init agular app with D3JS

--- app/topo.component.ts
+++ app/topo.component.ts
@@ -1,11 +1,13 @@
-import { Component, onChanges } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 @Component({
   selector: 'topo'
+  template: '<h1>My First Angular App</h1>'
 })
 
-export class TopoCompoment implements onChanges {
+export class TopoComponent implements OnChanges {
 
 ngOnChanges() {
+d3.select(element[0])
     //this.logs.push('OnChanges called !?!?!?');
     //throw new Error('ngOnChanges called; should not be when ngDoCheck is defined!');
   }
